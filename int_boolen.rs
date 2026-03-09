6852b81fb6e98a98164d70e5d6002cd34704790b
goodtoknow007 <goodtoknowyou007@gmail.com>
2026-03-09 15:46:00 +0530

    mutable variable

--- int_boolen.rs
+++ int_boolen.rs
@@ -1,7 +1,16 @@
-fn main() {
-    let x = 5;
-    let y = -2;
+// fn main() {
+//     let x = 5;
+//     let y = -2;
 
-    println!("value of x is {}",x);
-    println!("value of y is {}",y);
+//     println!("value of x is {}",x);
+//     println!("value of y is {}",y);
+// }
+
+fn main() {
+    let mut x = 0;
+
+    for i in 0..10 {
+        x = x + 1;
+        println!("value of x is {}",x);
+    }
 }
